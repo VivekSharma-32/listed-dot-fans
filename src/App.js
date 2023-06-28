9330967a83fe976a7fc2792c1b1ffa27db62b894
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-06-28 17:24:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
