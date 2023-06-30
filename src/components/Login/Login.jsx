f68403029b438a78f0632aa91f04986578509bba
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-06-30 11:06:21 +0530

    changes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,22 @@
 import "./Login.css";
 import Google from "../../images/google.svg";
 import Apple from "../../images/apple.svg";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleGoogleSignin = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then((res) => {
+      const credential = GoogleAuthProvider.credentialFromResult(res);
+      console.log(credential);
+      const user = res.user;
+      localStorage.setItem("user", user);
+      navigate("/dashboard");
+    });
+  };
   return (
     <div className="container" id="login">
       <div className="leftBox">
@@ -13,10 +27,10 @@ const Login = () => {
           <h3 className="loginHeading">Sign In</h3>
           <p className="smallText">Sign in to your account</p>
           <div className="loginButtons">
-            <div className="googleBtn">
+            <button className="googleBtn" onClick={handleGoogleSignin}>
               <img className="icon" src={Google} alt="google-icon" />
               Sign in with Google
-            </div>
+            </button>
             <div className="appleBtn">
               <img className="icon" src={Apple} alt="apple-icon" />
               Sign in with Apple
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDQHmcL0sWY5lnd-7dV15uFi0QMxD6ip6k",
+  authDomain: "react-fire-project-6e43a.firebaseapp.com",
+  databaseURL: "https://react-fire-project-6e43a-default-rtdb.firebaseio.com",
+  projectId: "react-fire-project-6e43a",
+  storageBucket: "react-fire-project-6e43a.appspot.com",
+  messagingSenderId: "839575957055",
+  appId: "1:839575957055:web:9c5c62eec87e4fcc9e128a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
